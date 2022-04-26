c31d62631ecd7272f3b58f23fa9e77b8364d9b3f
YoussefJell <youssef.jellouli@yahoo.com>
2022-04-26 10:20:42 +0000

    fix

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 for i in range(90, 64, -1):
-    letter = chr(i)
     if i % 2 == 0:
-        letter = letter.lower()
-    print(f"{letter}", end='')
+        i += 32
+    print(f"{chr(i)}", end='')
