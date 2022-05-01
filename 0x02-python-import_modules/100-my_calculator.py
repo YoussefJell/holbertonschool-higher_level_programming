5b100a35b9ddd2a25230fb1e7c1e63d16f4be67f
YoussefJell <youssef.jellouli@yahoo.com>
2022-05-01 11:03:30 +0000

    dict

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,31 +1,23 @@
 #!/usr/bin/python3
+from calculator_1 import add, div, sub, mul
+from sys import argv
+
 if __name__ == "__main__":
-    from calculator_1 import add, div, sub, mul
-    from sys import argv
     argc = len(argv) - 1
+    opers = {'+': add,
+             '-': sub,
+             '*': mul,
+             '/': div}
     if argc != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     else:
-        operator = argv[2]
         a = int(argv[1])
         b = int(argv[3])
-        if operator == '+':
-            res = add(a, b)
-            print(f"{a} + {b} = {res}")
-            exit(0)
-        elif operator == '-':
-            res = sub(a, b)
-            print(f"{a} - {b} = {res}")
-            exit(0)
-        elif operator == '*':
-            res = mul(a, b)
-            print(f"{a} * {b} = {res}")
-            exit(0)
-        elif operator == '/':
-            res = div(a, b)
-            print(f"{a} / {b} = {res}")
-            exit(0)
-        else:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
+        for key in opers:
+            if argv[2] == key:
+                operfunc = opers.get(argv[2])
+                print(f"{a} {argv[2]} {b} = {operfunc(a, b)}")
+                exit(0)
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
