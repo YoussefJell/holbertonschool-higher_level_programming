05c0286ab29ade9411fb932f94f75fa9548eca63
YoussefJell <youssef.jellouli@yahoo.com>
2022-05-02 01:53:30 +0000

    compress

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
+def tuple_func(my_Tuple=()):
+    if len(my_Tuple) < 2:
+        if len(my_Tuple) == 1:
+            my_Tuple = (my_Tuple[0], 0)
+        elif len(my_Tuple) == 0:
+            my_Tuple = (0, 0)
+    elif len(my_Tuple) > 2:
+        my_Tuple = (my_Tuple[0], my_Tuple[1])
+    return my_Tuple
+
+
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) < 2:
-        if len(tuple_a) == 1:
-            tuple_a = (tuple_a[0], 0)
-        elif len(tuple_a) == 0:
-            tuple_a = (0, 0)
-    elif len(tuple_a) > 2:
-        tuple_a = (tuple_a[0], tuple_a[1])
-
-    if len(tuple_b) < 2:
-        if len(tuple_b) == 1:
-            tuple_b = (tuple_b[0], 0)
-        elif len(tuple_b) == 0:
-            tuple_b = (0, 0)
-    elif len(tuple_b) > 2:
-        tuple_b = (tuple_b[0], tuple_b[1])
-
-    return ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
+    tuple_a = tuple_func(tuple_a)
+    tuple_b = tuple_func(tuple_b)
+    my_Tuple = ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
+    return my_Tuple
