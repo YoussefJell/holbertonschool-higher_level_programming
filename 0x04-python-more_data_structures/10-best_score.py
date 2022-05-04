5caa6ebb04dec4308edc150c799bd2f9c326bb35
YoussefJell <youssef.jellouli@yahoo.com>
2022-05-04 13:08:20 +0000

    add

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    HighestVal = max(a_dictionary.values())
+    HighestVal = max(a_dictionary.values(), default=None)
     for key, value in a_dictionary.items():
         if HighestVal == value:
             return key
