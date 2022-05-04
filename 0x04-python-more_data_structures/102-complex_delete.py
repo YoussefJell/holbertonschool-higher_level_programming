11a6a0299a2d991ca11fe5908d7e39c89ad85e7c
YoussefJell <youssef.jellouli@yahoo.com>
2022-05-04 19:58:56 +0000

    add

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from imp import new_module
+
+
+def complex_delete(a_dictionary, value):
+    new_list = []
+    for a_key, a_value in a_dictionary.items():
+        if a_value == value:
+            new_list.append(a_key)
+    for elem in new_list:
+        del a_dictionary[elem]
+    return a_dictionary
