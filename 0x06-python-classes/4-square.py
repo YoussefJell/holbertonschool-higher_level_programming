79564bb9aebed9780e31f24af67fbe4b4acad115
YoussefJell <youssef.jellouli@yahoo.com>
2022-05-17 01:23:16 +0000

    add

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Square class
+
+Defines a square class
+and has private instance size
+
+"""
+
+
+class Square:
+    def __init__(self, size=0):
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
