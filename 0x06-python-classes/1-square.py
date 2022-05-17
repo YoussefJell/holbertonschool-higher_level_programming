eaa4fff6660bb0dbf00fe3b7dcc74c1301644cbe
YoussefJell <youssef.jellouli@yahoo.com>
2022-05-17 14:48:36 +0000

    doc

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -8,6 +8,10 @@ and has private instance size
 
 
 class Square:
+    """Square Class
+    This square has a size
+    """
+
     def __init__(self, size):
         """__init__
         initializing square
