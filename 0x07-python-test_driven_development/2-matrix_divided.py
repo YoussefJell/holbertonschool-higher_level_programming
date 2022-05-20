5739cfcc1eb9854bbb2c3c5a15c0c346b5525b05
YoussefJell <youssef.jellouli@yahoo.com>
2022-05-20 13:18:44 +0000

    add

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""2-matrix_divided
+this module has the function matrix_divided
+"""
+
+
+def matrix_divided(matrix, div):
+    """matrix_divided
+    this will divide a matrix over the div argument
+    """
+    if type(matrix) is not list:
+        raise TypeError("matrix must be a matrix (list of lists)\
+of integers/floats")
+    for elem in matrix:
+        if type(elem) is not list:
+            raise TypeError("matrix must be a matrix (list of lists)\
+of integers/floats")
+
+    if type(div) not in (int, float):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    new_matrix = []
+    for e in matrix[:]:
+        new_matrix.append(list(map(lambda num: round((num / div), 2), e)))
+    return new_matrix
+
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6, 7]
+]
+print(matrix_divided(matrix, 4))
+print(matrix)
