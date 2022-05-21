52f5a6e437b8d18ecc552a929aff88a96354acdc
YoussefJell <youssef.jellouli@yahoo.com>
2022-05-21 12:33:25 +0000

    add

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -10,8 +10,8 @@ def say_my_name(first_name, last_name=""):
 
     Arg: first_name, last_name
 
-        first_name must be a string
-        last_name must be a string
+        • first_name must be a string
+        • last_name must be a string
     """
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
