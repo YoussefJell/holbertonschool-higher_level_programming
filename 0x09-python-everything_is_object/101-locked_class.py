ca52e424900373e3dfb34f15b594e2c591110faa
YoussefJell <youssef.jellouli@yahoo.com>
2022-05-24 17:54:00 +0000

    Documentation and prevents the user from dynamically creating new instance attributes except if it's 'first_name'

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
+"""LockedClass
+This class only allows first_name field
+"""
+
+
 class LockedClass(object):
+    """LockedClass Class
+    This class only allows first_name field
+    """
     __slots__ = 'first_name'
+
+    def __init__(self, first_name="") -> None:
+        self.first_name = first_name
