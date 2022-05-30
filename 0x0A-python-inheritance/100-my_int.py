7ecaf37048857465ece6043f89e1143221e31982
YoussefJell <youssef.jellouli@yahoo.com>
2022-05-30 19:28:05 +0000

    add

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -4,8 +4,13 @@ this module swaps Eq and Ne, EVIL!"""
 
 
 class MyInt(int):
+    """MyInt
+    this is myint class
+    """
+
     def __eq__(self, other):
         """eq swap"""
+
         return super().__ne__(other)
 
     def __ne__(self, other):
