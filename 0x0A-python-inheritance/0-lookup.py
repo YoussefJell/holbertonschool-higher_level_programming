ed72a363612738c991bb3a1a9abd3b378cc2e3fd
YoussefJell <youssef.jellouli@yahoo.com>
2022-05-30 17:19:52 +0000

    Changing Doc

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""lookup function
+returns dir of obj
+"""
+
+
 def lookup(obj):
-    """lookup function
-    return dir() in list format
-    """
+    """returns the list of available attributes and methods of an object"""
     return dir(obj)
