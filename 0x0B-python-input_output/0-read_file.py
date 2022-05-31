b03ac71da77255f44e90700929ae980d43a29b7d
YoussefJell <youssef.jellouli@yahoo.com>
2022-05-31 15:58:54 +0000

    Potential fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,4 +8,5 @@ def read_file(filename=""):
     """
 
     with open(filename, encoding="utf-8") as my_file:
-        print(my_file.read())
+        for line in my_file:
+            print(line, end='')
