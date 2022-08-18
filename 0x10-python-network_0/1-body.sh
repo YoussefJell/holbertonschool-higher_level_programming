89dabb7ec7c0ce6145ac385a1eb51011af51d602
YoussefJell <youssef.jellouli@yahoo.com>
2022-08-18 11:51:28 +0000

    add

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,7 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # displays the size of the body of the response if the http_code is 200 OK
-arg=$1
-STATUS=$(curl -s -L -w '%{http_code}' "$arg" -o /dev/null)
-if [ "$STATUS" -eq 200 ]; then
-        curl -sL "$arg"
-fi
+arg=$1; if [ $(curl -s -L -w '%{http_code}' "$arg" -o /dev/null) -eq 200 ]; then curl -sL "$arg"; fi
