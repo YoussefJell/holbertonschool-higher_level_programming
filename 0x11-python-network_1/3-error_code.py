dd7694eaeec73c1c629b3f787f7d5a179ac30d8b
YoussefJell <youssef.jellouli@yahoo.com>
2022-08-19 11:45:44 +0000

    add

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -12,4 +12,4 @@ if __name__ == "__main__":
             html = my_url.read()
             print(html.decode('utf-8'))
     except urllib.error.HTTPError as e:
-        print('Error code:' + e.code)
+        print('Error code: ' + e.code)
--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     url = argv[1]
     r = requests.get(url)
     if (r.status_code >= 400):
-        print(r.status_code)
+        print("Error code: " + r.status_code)
     else:
         print(r.text)
