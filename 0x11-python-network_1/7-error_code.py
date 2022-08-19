70153f5c3577e4a85c1014913a1c4283592bac8f
YoussefJell <youssef.jellouli@yahoo.com>
2022-08-19 11:49:24 +0000

    add

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""4-hbtn_status Module"""
+"""7-error_code Module"""
 import requests
 from sys import argv
 
 if __name__ == "__main__":
     url = argv[1]
     r = requests.get(url)
-    if (r.status_code >= 400):
-        print("Error code: " + r.status_code)
+    if r.status_code >= 400:
+        print("Error code:", r.status_code)
     else:
         print(r.text)
