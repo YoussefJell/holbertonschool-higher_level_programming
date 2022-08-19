b19490ae5ee754314d4fb1dc88851f2f228c3b8c
YoussefJell <youssef.jellouli@yahoo.com>
2022-08-19 15:41:20 +0000

    add

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,12 +5,12 @@ from sys import argv
 
 if __name__ == "__main__":
 
-    if len(argv[1]) == 1:
+    if len(argv[1]) > 1:
         myobj = {'q': argv[1]}
     else:
-        myobj = {'q': ""}
+        myobj = {'q': ''}
 
-    url = 'http://0.0.0.0:5000/search_user'
+    url = 'http://eebab4f62ae4.6ef63e22.hbtn-cod.io:5000/search_user'
     try:
         r = requests.post(url, myobj).json()
         if r.get('id') and r.get('name'):
