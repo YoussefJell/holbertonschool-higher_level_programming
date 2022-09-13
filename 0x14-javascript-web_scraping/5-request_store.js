24322653e91e4675c8f05aca5edd3fbee48cd9f1
YoussefJell <youssef.jellouli@yahoo.com>
2022-09-13 13:37:47 +0000

    semicolon

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
 const axios = require('axios').default;
-const reqUrl = process.argv[2]
+const reqUrl = process.argv[2];
 axios.get(reqUrl)
   .then(function (response) {
     fs.writeFile(process.argv[3], response.data, (err, data) => {
