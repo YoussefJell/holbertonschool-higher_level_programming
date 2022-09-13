f70f7f4f5ab59f3f66e1832b465c5e2454dee76c
YoussefJell <youssef.jellouli@yahoo.com>
2022-09-13 13:29:33 +0000

    error

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,14 +1,16 @@
 #!/usr/bin/node
 const axios = require('axios').default;
 const reqUrl = process.argv[2];
-const id = '18';
 let count = 0;
 axios.get(reqUrl)
   .then(function (response) {
     for (let i = 0; i < response.data.count; i++) {
-      if (String(response.data.results[i].characters).includes(id)) {
+      if (String(response.data.results[i].characters).includes("18")) {
         count = count + 1;
       }
     }
     console.log(count);
+  })
+  .catch(function (error) {
+    console.error(error);
   });
