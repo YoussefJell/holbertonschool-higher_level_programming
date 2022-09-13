0413f77b45231bdab6020ec7256a3632a8ad8c68
YoussefJell <youssef.jellouli@yahoo.com>
2022-09-13 21:25:24 +0000

    change to nested loop

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,8 +5,9 @@ let count = 0;
 axios.get(reqUrl)
   .then(function (response) {
     for (let i = 0; i < response.data.count; i++) {
-      if (String(response.data.results[i].characters).includes('18')) {
-        count = count + 1;
+      const arr = response.data.results[i].characters;
+      for (let j = 0; j < arr.length; j++) {
+        if (arr[j].includes('18')) { count += 1; }
       }
     }
     console.log(count);
