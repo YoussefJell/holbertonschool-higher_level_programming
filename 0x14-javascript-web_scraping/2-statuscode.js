45fde92255ed7a66a53444b42520e21c373e184c
YoussefJell <youssef.jellouli@yahoo.com>
2022-09-13 12:27:53 +0000

    add

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,8 +2,8 @@
 const axios = require('axios').default;
 axios.get(process.argv[2])
   .then(function (response) {
-    console.log('code: %d', response.status)
+    console.log('code: %d', response.status);
   })
-  .catch(function (error) {
+  .catch(function () {
     console.log('code: 404');
   });
