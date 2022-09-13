65ede1dd4ce4f5d3e2660d43299653682a68973d
YoussefJell <youssef.jellouli@yahoo.com>
2022-09-13 13:25:19 +0000

    add

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 const axios = require('axios').default;
 const reqUrl = process.argv[2];
+const id = '18';
 let count = 0;
 axios.get(reqUrl)
   .then(function (response) {
     for (let i = 0; i < response.data.count; i++) {
-      if (String(response.data.results[i].characters).includes('18')) {
+      if (String(response.data.results[i].characters).includes(id)) {
         count = count + 1;
       }
     }
