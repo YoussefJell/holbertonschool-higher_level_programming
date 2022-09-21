ca7017584d7e939b3ec11a6278cf70034ccd6ad6
YoussefJell <youssef.jellouli@yahoo.com>
2022-09-21 13:24:14 +0000

    Fix

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,5 +1,7 @@
 $(function () {
   $.get("https://swapi-api.hbtn.io/api/people/5/?format=json", function (data) {
-    $('DIV#character').text(data.name);
+    for (const movie of data.results) {
+      $('UL#list_movies').append(`<li>${movie.title}</li>`);
+    }
   });
 });
